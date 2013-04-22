277ca5ba873b1bbe08a22b05e51b38b4f483e4ec
amaia <alexandre.maia@gmail.com>
2013-04-22 00:52:49 -0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FfmpegRails::Application.config.secret_token = '2721426f1bfba0a7061d4608a3b764dcd50acacc363724955cfa5637f663c5a746649598309e8a9d554740fc97eda6fa1a1a16a63842f76892bdcfefb2b7c149'
